b10e5aad134bd474a9253c4d5e72935e2145d51f
Ahmed <Ahmed9588406@gmail.com>
2024-02-04 21:01:33 +0200

    Moved Mindspark to root Route

--- parrot/src/app/Mindspark/page.tsx
+++ parrot/src/app/Mindspark/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link"
 
 export default function Home (){
     return(
-        <main className="flex flex-col h-screen items-center justify-center gap-5 text-white">
+        <main className="flex flex-col h-full items-center justify-center gap-5 text-white">
             <div className="flex items-center gap-4">
                 <Image src={logo} alt="MindSpark logo" width={100} height={100} />
                 <span className="font-extrabold tracking-tight text-4xl lg:text-5xl">MindSpark</span>
@@ -14,7 +14,7 @@ export default function Home (){
                 An inteligent note-taking app ai integration,built with OpenAI, pinecone, Next.js, shadcnUi, Clerk and more 
             </p>
             <Button size="lg" asChild>
-                <Link href="MindSpark/notes">Open </Link>
+                <Link href="Mindspark/notes">Open </Link>
             </Button>
         </main>
     );

e02a82c141222094e9e2ba0bb771009b87a02c02
PanceeAyad <penseeayad22@gmail.com>
2024-02-09 18:47:44 +0200

    Co-authored-by: Ahmed  <Ahmed9588406@users.noreply.github.com> Co-authored-by: Dounia173 <Dounia173@users.noreply.github.com> Co-authored-by: ziad-kamel <ziad-kamel@users.noreply.github.com>

--- parrot/src/app/audiogeneration/page.tsx
+++ parrot/src/app/audiogeneration/page.tsx
@@ -15,13 +15,41 @@ export default function audioGenerationpPage() {
             placeholder="Let me know what's on your mind"
             className="w-[35rem] text-center rounded-[2rem] bg-[#D9D9D9]"
           />
-          <Button className="w-56 h-14 rounded-[2rem] font-jura text-2xl bg-gradient-to-r from-[#ec80f6] from-30% to-black to-[125%] shadow-xl  ">
-            Music
-          </Button>
-          <Button className="w-56 h-14 rounded-[2rem] font-jura text-2xl bg-gradient-to-r from-[#ec80f6] from-30% to-black to-[125%] shadow-xl  ">
-            SFX
-          </Button>
-< div className=" w-[60%] mt-8 mb-10" ></div>
+          <div className="flex justify-center" style={{ width: "47rem" }}>
+            <div className="flex flex-row justify-center space-y-80 w-full h-80 ">
+              <div className="flex flex-col justify-evenly w-3/5 ">
+                <div className="flex flex-row justify-around">
+                  <Button className="w-54 h-14 rounded-[3rem] font-jura text-2xl bg-gradient-to-r from-[#f052ea] from-10% to-black to-[120%] shadow-xl  ">
+                    Music
+                  </Button>
+                  <Button className="w-54 h-15 rounded-[3rem] font-jura text-2xl bg-gradient-to-r from-[#ce51ed] from-10% to-black to-[120%] shadow-xl  ">
+                    SFX
+                  </Button>
+                </div>
+                <div className="flex flex-col justify-evenly w-[30rem] h-[15rem] rounded-md bg-[#382837]">
+                  <div className=" rounded-md flex justify-stretch ">
+                    <div className="flex flex-row justify-around gap-4">
+                      <button className="w-40 h-14 rounded-[2rem] bg-[#D9D9D9]">
+                        audio_1
+                      </button>
+                      <button className="w-40 h-14 rounded-[2rem] bg-[#D9D9D9]">
+                        audio_2
+                      </button>
+                    </div>
+
+                    <div className="flex flex-row justify-around gap-4">
+                      <button className="w-40 h-14 rounded-[2rem] bg-[#D9D9D9]">
+                        audio_3
+                      </button>
+                      <button className="w-40 h-14 rounded-[2rem] bg-[#D9D9D9]">
+                        audio_4
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>

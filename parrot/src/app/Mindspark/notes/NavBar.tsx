22a5b92c780851a07e0e9c879ce42dbab60e293c
Donia Samy Mohamed Hussien <sci.doniasamy95863@alexu.edu.eg>
2024-02-09 19:10:52 +0200

    ai chat box added

--- parrot/src/app/Mindspark/notes/NavBar.tsx
+++ parrot/src/app/Mindspark/notes/NavBar.tsx
@@ -1,51 +1,61 @@
-"use client" // made this because we can only use useState in the client component default component in next.js is server component
-import logo from "@/assets/logo.png"
-import AddEditNoteDialog from "@/components/AddEditNoteDialog"
-import ThemeToggleButton from "@/components/ThemeToggleButton"
-import { Button } from "@/components/ui/button"
-import { UserButton } from "@clerk/nextjs"
-import { Plus } from "lucide-react"
-import Image from "next/image"
-import Link from "next/link"
-import { useState } from "react"
+"use client"; // made this because we can only use useState in the client component default component in next.js is server component
+import logo from "@/assets/logo.png";
+import AddEditNoteDialog from "@/components/AddEditNoteDialog";
+import ThemeToggleButton from "@/components/ThemeToggleButton";
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { Plus } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 
-import { dark } from "@clerk/themes"
-import { useTheme } from "next-themes"
-export default function NavBar(){
-    const {theme} = useTheme()
+import { dark } from "@clerk/themes";
+import { useTheme } from "next-themes";
+import AIChatButton from "@/components/AIChatButton";
+export default function NavBar() {
+  const { theme } = useTheme();
   // we want to render the message when we use Add Note button in navbar
-  const [showAddEditNoteDialog, setShowAddEditNoteDialog] = useState(false)
+  const [showAddEditNoteDialog, setShowAddEditNoteDialog] = useState(false);
 
+  return (
+    <div>
+      <div className="p-4 shadow">
+        <div className="max-w-7xl m-auto flex flex-wrap gap-3 items-center justify-between">
+          <Link href="/notes" className="flex items-center gap-1">
+            <Image
+              src={logo}
+              alt="MindSpark logo"
+              width={40}
+              height={40}
+            ></Image>
+            <span className="font-medium text-white">MindSpark</span>
+          </Link>
+          <div className="flex items-center gap-2">
+            <UserButton
+              afterSignOutUrl="/"
+              appearance={{
+                baseTheme: theme === "dark" ? dark : undefined,
+                elements: { avatarBox: { width: "2.5rem", height: "2.5rem" } },
+              }}
+            />
 
-return (
-  <div>
-<div className="p-4 shadow">
-  
-  <div className="max-w-7xl m-auto flex flex-wrap gap-3 items-center justify-between">
-    <Link href="/notes" className="flex items-center gap-1">
-    <Image  src={logo} alt="MindSpark logo" width={40} height={40} ></Image>
-    <span className="font-medium text-white">MindSpark</span>
-    </Link>
-   <div className="flex items-center gap-2">
-    <UserButton afterSignOutUrl="/"
-    appearance={{
-      baseTheme: (theme === "dark" ? dark: undefined),
-      elements: {avatarBox:{width:"2.5rem", height:"2.5rem"}}
-    }}    
-    />
+            <ThemeToggleButton />
 
-    <ThemeToggleButton />
-
-    <Button className="font-semibold" onClick={()=> setShowAddEditNoteDialog(true)}>
-      <Plus size={20} className="mr-2" />
-      Add Note
-    </Button>
-    
-   </div>
-  </div>
-  </div>
-  <AddEditNoteDialog open={showAddEditNoteDialog} setOpen={setShowAddEditNoteDialog} />
-
-  </div>
-)
+            <Button
+              className="font-semibold"
+              onClick={() => setShowAddEditNoteDialog(true)}
+            >
+              <Plus size={20} className="mr-2" />
+              Add Note
+            </Button>
+            <AIChatButton />
+          </div>
+        </div>
+      </div>
+      <AddEditNoteDialog
+        open={showAddEditNoteDialog}
+        setOpen={setShowAddEditNoteDialog}
+      />
+    </div>
+  );
 }

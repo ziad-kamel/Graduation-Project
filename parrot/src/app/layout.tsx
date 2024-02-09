ddba5aa60073d658a89feb228d16275e9261cde8
PanceeAyad <penseeayad22@gmail.com>
2024-02-09 15:38:41 +0200

    audiogeneration page

--- parrot/src/app/layout.tsx
+++ parrot/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Image from "next/image";
 import "./globals.css";
+import {ClerkProvider} from "@clerk/nextjs"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +17,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Image src={'/logo.png'} alt="parrot pic" draggable={false} width={80} height={80} style={{position:"fixed", right:"1rem" , bottom:"1rem"}}/>
+
+      </body>
     </html>
+    </ClerkProvider>
   );
 }

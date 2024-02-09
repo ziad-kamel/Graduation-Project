22553c570ca200913f63c3a8fad3374a6097417e
PanceeAyad <penseeayad22@gmail.com>
2024-02-09 20:35:59 +0200

    audiogeneration page

--- parrot/src/app/audiogeneration/page.tsx
+++ parrot/src/app/audiogeneration/page.tsx
@@ -11,12 +11,8 @@ export default function audioGenerationpPage() {
           <h1 className="text-5xl font-bold text-white font-jura">
             Generative Audio
           </h1>
-          <Input
-            placeholder="Let me know what's on your mind"
-            className="w-[35rem] text-center rounded-[2rem] bg-[#D9D9D9]"
-          />
-          <div className="flex justify-center" style={{ width: "47rem" }}>
-            <div className="flex flex-row justify-center space-y-80 w-full h-80 ">
+          <div className="flex justify-center" style={{ width: "53rem" }}>
+            <div className="flex flex-row justify-center space-y-15 w-full h-50 ">
               <div className="flex flex-col justify-evenly w-3/5 ">
                 <div className="flex flex-row justify-around">
                   <Button className="w-54 h-14 rounded-[3rem] font-jura text-2xl bg-gradient-to-r from-[#f052ea] from-10% to-black to-[120%] shadow-xl  ">
@@ -26,25 +22,30 @@ export default function audioGenerationpPage() {
                     SFX
                   </Button>
                 </div>
-                <div className="flex flex-col justify-evenly w-[30rem] h-[15rem] rounded-md bg-[#382837]">
-                  <div className=" rounded-md flex justify-stretch ">
-                    <div className="flex flex-row justify-around gap-4">
-                      <button className="w-40 h-14 rounded-[2rem] bg-[#D9D9D9]">
-                        audio_1
-                      </button>
-                      <button className="w-40 h-14 rounded-[2rem] bg-[#D9D9D9]">
-                        audio_2
-                      </button>
-                    </div>
-
-                    <div className="flex flex-row justify-around gap-4">
-                      <button className="w-40 h-14 rounded-[2rem] bg-[#D9D9D9]">
-                        audio_3
-                      </button>
-                      <button className="w-40 h-14 rounded-[2rem] bg-[#D9D9D9]">
-                        audio_4
-                      </button>
-                    </div>
+                <Input
+                  placeholder="Let me know what's on your mind"
+                  className="w-[35rem] text-center rounded-[2rem] bg-[#D9D9D9] mt-5"
+                />
+                <div
+                  className="flex flex-col w-[32rem] h-[11rem] rounded-md justify-stretch bg-[#382837] mt-10"
+                  style={{ width: "34.5rem" }} 
+                >
+                  <div className="flex flex-row justify-around gap-4 mt-5">
+                    <button className="w-40 h-14 rounded-[2rem] bg-[#D9D9D9]">
+                      audio_1
+                    </button>
+                    <button className="w-40 h-14 rounded-[2rem] bg-[#D9D9D9]">
+                      audio_3
+                    </button>
+                  </div>
+                  
+                  <div className="flex flex-row justify-around gap-4 mt-5">
+                    <button className="w-40 h-14 rounded-[2rem] bg-[#D9D9D9]">
+                      audio_2
+                    </button>
+                    <button className="w-40 h-14 rounded-[2rem] bg-[#D9D9D9]">
+                      audio_4
+                    </button>
                   </div>
                 </div>
               </div>

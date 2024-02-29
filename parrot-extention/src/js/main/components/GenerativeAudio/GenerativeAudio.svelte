ef64db33e6f032a4284a1ab54b777e9a14fb88ef
Ahmed Gamal <sci.ahmedgamal40520@alexu.edu.eg>
2024-02-29 14:49:25 +0200

    Integrating API not ready

--- parrot-extention/src/js/main/components/GenerativeAudio/GenerativeAudio.svelte
+++ parrot-extention/src/js/main/components/GenerativeAudio/GenerativeAudio.svelte
@@ -1,50 +1,132 @@
-<script>
+<script >
   import './GenerativeAudio.scss';
+  import {
+    csi,
+    evalES,
+    evalTS,
+    subscribeBackgroundColor
+  } from "../../../lib/utils/bolt";
 
-  const audioGenModel = [
-    'model 1',
-    'model 2',
-    'model 3',
-    'model 4',
-  ]
-  const audioGenStrategy = [
-    'strategy 1',
-    'strategy 2',
-    'strategy 3',
-    'strategy 4',
-  ]
+  import {
+    Audio_Generation_Version,
+    Normalization_Strategy,
+  } from "../../../lib/utils/types";
+
+  import Replicate from "replicate";
+  
+
+  let prompt;
+  let version;
+  let strategy;
+  let duration =1;
+  let output = "";
+  // alert(process.env.REPLICATE_API_TOKEN)
+
+  const generateAudio = async () => {
+    alert(`Prompt: ${prompt}\nVersion: ${version}\nStrategy: ${strategy}\nDuration: ${duration}`);
+    try {
+      const replicate = new Replicate({
+        auth: "r8_d1g6LBLBi8Xyg45nbWVhA0baM9Xtttw4KBEox",
+      });
+      const res = await replicate.run(
+        "meta/musicgen:b05b1dff1d8c6dc63d14b0cdb42135378dcb87f6373b0d3d341ede46e59e2b38",
+        {
+          input: {
+            prompt: prompt,
+            duration: duration,
+            model_version: version,
+            normalization_strategy: strategy,
+          }
+        }
+      );
+      output = res.output;
+    } catch (e) {
+      alert(e.message);
+    }
+  };
+
+  const https = require('https');
+  const fs = require('fs');
+  function a() {
+        // use https post request with the data
+        const data = JSON.stringify({
+          version: "b05b1dff1d8c6dc63d14b0cdb42135378dcb87f6373b0d3d341ede46e59e2b38",
+          input: {
+            prompt: prompt,
+            duration: duration,
+            model_version: version,
+            normalization_strategy: strategy
+          }
+        });
+        const options = {
+          hostname: 'api.replicate.com',
+          port: 443,
+          path: '/v1/predictions',
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': data.length,
+            'Authorization': 'Token r8_d1g6LBLBi8Xyg45nbWVhA0baM9Xtttw4KBEox',
+          },
+          body: data
+        };
+        alert(options);
+        try {
+          const req = https.request(options, res => {
+            alert(`statusCode: ${res.statusCode}`);
+            res.on('data', d => {
+              process.stdout.write(d);
+            });
+            output = res.output;
+          });
+        } catch (e) {
+          alert(e.message);
+        }
+        alert("req");
+        
+
+
+
+      };
 </script>
 
 
 
 <div class="generative-content">
     <div >
-      <p style = "display: inline">Prompt: </p><textarea></textarea>
+      <p style = "display: inline">Prompt: </p><textarea bind:value={prompt}></textarea>
     </div>
 
     <div class="audio-gen-options">
       <div>
         <p style = "display: inline">Version: </p>
-          <select> 
-              {#each audioGenModel as modelName}
-                <option>{modelName}</option>
+          <select bind:value={version}> 
+              {#each Audio_Generation_Version as modelName}
+                <option>{modelName.model_version}</option>
               {/each}
           </select>
       </div>
 
       <div>
         <p style = "display: inline"> Strategy: </p>
-          <select> 
-            {#each audioGenStrategy as strategy}
-              <option>{strategy}</option>
+          <select bind:value={strategy}> 
+            {#each Normalization_Strategy as strategy}
+              <option>{strategy.strategy}</option>
             {/each}
           </select>
       </div>
       <div>
-        <p style = "display: inline">Duration: </p><input type="number" min=3 max=20 />
+        <p style="display: inline">Duration: </p><input bind:value={duration} type="number" min=3 max=20 />
       </div>
     </div>
     <div class="generate-btn">
-      <button class= "sidebar-button" type="submit" >Generate</button>
+      <button class= "sidebar-button" type="submit" on:click={() =>{a()}} >Generate</button>
     </div>
+    <div class="audio-output">
+      <p>Output:</p> <p>{output}</p>
+      <audio controls>
+        <source src={output} type="audio/mpeg">
+        Your browser does not support the audio element.
+      </audio>
+    </div>  
   </div>

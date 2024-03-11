94f0456283a7c25e91ec3ac793d9c546118a7696
ziad <ziad.mohamed.kamell.184@gmail.com>
2024-03-11 14:36:32 +0200

    fix: audio cleanup generation error

--- parrot/src/app/hooks/usePostAudioCleanup.ts
+++ parrot/src/app/hooks/usePostAudioCleanup.ts
@@ -1,31 +1,3 @@
-// import { customeInput } from "@/lib/types/AudioCleanuptypes";
-// import { useState } from "react";
-
-// const usePostAudioCleanup = () => {
-//   const [isLoading, setIsLoading] = useState(false);
-//   const AudioCleanup = async (FileUrl: string) => {
-//     setIsLoading(true);
-//     const inputs: customeInput = {
-//       audioURL: FileUrl,
-//     };
-//     return await fetch("/audioCleanup/api", {
-//       method: "POST",
-//       body: JSON.stringify({ inputs }),
-//       headers: { "Contemt-Type": "application/json" },
-//     })
-//       .then(async (res) => {
-//         setIsLoading(false);
-//         return await res.json();
-//       })
-//       .catch((error) => {
-//         setIsLoading(false);
-//         alert("Error in API Call : ${error.message}");
-//       });
-//   };
-//   return { isLoading, AudioCleanup };
-// };
-// export default usePostAudioCleanup;
-
 import { customeInput } from "@/lib/types/AudioCleanuptypes";
 import { useState } from "react";
 
@@ -37,25 +9,20 @@ const usePostAudioCleanup = () => {
     const inputs: customeInput = {
       audioURL: FileUrl,
     };
-    try {
-      const response = await fetch("/generation/audioCleanup/api", {
-        method: "POST",
-        body: JSON.stringify({ inputs }),
-        headers: { "Content-Type": "application/json" },
-      });
 
-      if (!response.ok) {
-        throw new Error("Failed to fetch data");
-      }
-
-      const responseData = await response.json();
+    return await fetch('/generation/audioCleanup/api', {
+      method: 'POST',
+      body: JSON.stringify({inputs}),
+      headers: { "Content-Type": "application/json" },
+    })
+    .then(async (res)=>{
       setIsLoading(false);
-      return responseData;
-    } catch (error: any) {
-      // Explicitly specify the type as 'any'
+      return await res.json();
+    })
+    .catch((error)=>{
       setIsLoading(false);
-      alert(`Error in API Call: ${error.message}`);
-    }
+      alert(`Error in API Call : ${error.message}`);
+    });
   };
 
   return { isLoading, AudioCleanup };
